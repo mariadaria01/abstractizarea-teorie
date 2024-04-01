46789a245a7f34bb9255e590df75cf9b1c07d307
mariadaria01 <20tmd.daria04@gmail.com>
2024-04-01 10:26:54 +0300

    Teorie abstractizare

---
+++ teorie/Program.cs
@@ -0,0 +1,48 @@
+﻿using teorie;
+
+public abstract class Animal
+{
+    public abstract void MakeSound();
+
+    public void Sleep()
+    {
+        Console.WriteLine("zzz..");
+    }
+}
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("The dog barks.");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("The cat meows.");
+    }
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        /*Animal animal = new Dog();
+        animal.MakeSound();
+        animal.Sleep();*/
+        
+        Square square = new Square(0, 0, 10);
+        Console.WriteLine(square.CalculateArea());
+        square.Draw();
+        square.Resize(0.5);
+        Console.WriteLine(square.CalculatePerimeter());
+
+        Circle circle = new Circle(5, 5, 4);
+        Console.WriteLine(circle.CalculateArea());
+        circle.Draw();
+        circle.Resize(0.5);
+        Console.WriteLine(circle.CalculatePerimeter());
+    }
+}
---
+++ teorie/Shape.cs
@@ -0,0 +1,107 @@
+﻿namespace teorie;
+
+public interface IShape
+{
+    double CalculateArea();
+    double CalculatePerimeter();
+}
+
+public interface IDrawable
+{
+    void Draw();
+}
+
+public interface IResizeable
+{
+    void Resize(double factor);
+}
+
+public class Coordinates
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Coordinates(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override string ToString()
+    {
+        return $"{X}:{Y}";
+    }
+}
+
+public abstract class Shape : IShape, IDrawable, IResizeable
+{
+    public Coordinates Coords { get; set; }
+
+    public Shape(int x, int y)
+    {
+        Coords = new Coordinates(x, y);
+    }
+
+    public abstract double CalculateArea();
+    public abstract double CalculatePerimeter();
+    public abstract void Draw();
+    public abstract void Resize(double factor);
+}
+
+public class Square : Shape
+{
+    public double Length { get; set; }
+
+    public Square(int x, int y, double length) : base(x,y)
+    {
+        Length = length;
+    }
+
+    public override double CalculateArea()
+    {
+        return Length * Length;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 4 * Length;
+    }
+
+    public override void Draw()
+    {
+        Console.WriteLine($"Drawn square at {Coords}");
+    }
+
+    public override void Resize(double factor)
+    {
+        Length *= factor;
+    }
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+    
+    public Circle(int x, int y, double radius) : base(x,y)
+    {
+        Radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Radius * Radius * Math.PI;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return Radius * 2 * Math.PI;
+    }
+    public override void Draw()
+    {
+        Console.WriteLine($"Drawn circle at {Coords}");
+    }
+    public override void Resize(double factor)
+    {
+        Radius *= factor;
+    }
+}
